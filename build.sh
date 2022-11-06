fc8c6b177904c0b894055b4badea7b33fb95b424
yuantingwei <yuantingwei@arcsparrow.com>
2022-11-06 13:24:20 +0800

    chore[CICD]: docker build image mofification for github action To let github action can build/push image to ghcr.io. I change Dockerfile receipt to correct binary path to add to docker image.

--- build.sh
+++ build.sh
@@ -6,10 +6,12 @@ GO_BUILD_TAGS=
 OUTPUT_SUBDIR=
 export GOOS=linux
 #export CGO_ENABLED=1
-if [ "$TARGET" = "cloud" ]; then
-  export GOARCH=amd64
-  GO_BUILD_TAGS=""
+export GOARCH=amd64
+GO_BUILD_TAGS=""
+if [ "$TARGET" = "github_action" ]; then
   go build -v -o="./load_balancer/release/eth_block_indexer"
 else
   go build -v -o="./load_balancer/release/eth_block_indexer"
+  docker build -t eth_block_indexer_indexer:v0.3 -f load_balancer/Dockerfile_indexer .
+  docker build -t eth_block_indexer_http_api:v0.3 -f load_balancer/Dockerfile_http_api .
 fi
